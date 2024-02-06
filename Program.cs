b95f86bb759a18fb39c4585f59ff2a282ea83b3d
kallam anantha kotireddy <kallamananthakoti@gmail.com>
2024-02-06 16:07:46 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace file_watcher_tool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
